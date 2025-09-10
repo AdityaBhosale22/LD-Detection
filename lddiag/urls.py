ba0bc7c75ed90eb2ea4164cbb3676c6a197b8dca
Aditya Bhosale <adityabhosale4388@gmail.com>
2025-09-10 21:30:21 +0530

    UI polish: improved base layout, navbar, static CSS, template home page

--- lddiag/urls.py
+++ lddiag/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
-def home(_request):
-	return HttpResponse('<a href="/accounts/login/">Login</a> | <a href="/accounts/register/">Register</a> | <a href="/assessments/intake/">LD Intake</a> | <a href="/assessments/math/start/">Math</a> | <a href="/assessments/grammar/start/">Grammar</a> | <a href="/assessments/reading/start/">Reading</a> | <a href="/assessments/memory/start/">Memory</a> | <a href="/assessments/scenario/start/">Scenario</a> | <a href="/recommendations/">Recommendations</a> | <a href="/reports/dashboard/">Analytics</a> | <a href="/reports/download/">Download Report</a>')
+def home(request):
+	return render(request, "home.html")
 
 
 urlpatterns = [
